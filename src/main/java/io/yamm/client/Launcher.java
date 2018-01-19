c17ea2f566aa8e7a36d66419dfade0ba25279732
Benjamin Howe <ben@bh96.uk>
2018-01-19 22:21:47 +0000

    Reworked package into a single JAR using JDotSoft JarClassLoader.

---
+++ src/main/java/io/yamm/client/Launcher.java
@@ -0,0 +1,16 @@
+package io.yamm.client;
+
+import com.jdotsoft.jarloader.JarClassLoader;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        JarClassLoader jcl = new JarClassLoader();
+        try {
+            jcl.invokeMain("io.yamm.client.GUI", args);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    } // main()
+
+} // class MyAppLauncher
